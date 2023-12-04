b138834b65fb45273ca912098785742a6277557b
Troy <q133457@gmail.com>
2023-12-04 10:36:02 +0800

    ?

---
+++ 2147.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numberOfWays(self, corridor: str) -> int:
+        
+        cs=corridor.count("S")
+        if not cs or cs%2:
+          return 0
+        l=corridor.split("S")[2:-2:2]
+        ans=1
+        for i in l:
+          ans*=(len(i)+1)
+        return ans%(10**9+7)
